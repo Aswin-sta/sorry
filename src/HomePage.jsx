772a95129ee5fb0c42bf7bc0e4008b9008083a6b
Aswinkumar TS <96405492+Aswin-sta@users.noreply.github.com>
2024-08-02 17:27:49 +0530

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import CustomModal from "./CustomModal";
-import sound from "src/sound.mp3"; // Import the new sound file
+import sound from "./sound.mp3"; // Import the new sound file
 
 const TypingText = ({ text, onComplete }) => {
   const [displayedText, setDisplayedText] = useState("");
